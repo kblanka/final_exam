d433a02b99525cb85f8591f3e68b8d4c6b8fb072
kblanka <kovacsblanka06@gmail.com>
2017-02-17 09:44:43 +0100

    Resolved with vectors

--- Exercise_10/main.cpp
+++ Exercise_10/main.cpp
@@ -1,26 +1,55 @@
+//Exercise 10:
+//Create a function that takes an array of integers and returns the average of the even numbers from that array
+
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
 
 using namespace std;
 
-int averageof_even_num(int* input_array, int array_length) {
+void fill_vector_random(vector<int>& newVector);
+
+int averageof_even_num(vector<int>& newVector);
+
+void print_vector(vector<int>& newVector);
+
+int main() {
+  vector<int> myVector;
+  myVector.reserve(10);
+  fill_vector_random(myVector);
+  print_vector(myVector);
+  cout << averageof_even_num(myVector) <<endl;
+  
+  return 0;
+}
+
+void fill_vector_random(vector<int>& newVector) {
+  srand(time(NULL));
+  for (int i = 0; i < 10; i++) {
+    int b = rand() % 10;
+    newVector.push_back(b);
+  }
+}
+
+int averageof_even_num(vector<int>& newVector) {
   int total = 0;
   int count = 0;
-  for (int i = 0; i < array_length; i++) {
-    if (input_array[i] % 2 == 0) {
-      total += input_array[i];
+  for (int i = 0; i < newVector.size(); i++) {
+    if (newVector[i] % 2 == 0) {
+      total += newVector[i];
       count++;
     }
   }
   return total / count;
 }
 
-int main() {
-  int my_array[] = { 1, 2, 3, 4, 5, 6 };
-  cout << averageof_even_num(my_array, 6) << endl;
-  return 0;
+void print_vector(vector<int>& newVector) {
+  cout << "The elements of the randomly generated vector: ";
+  for (unsigned int i = 0; i < newVector.size(); i++) {
+    cout << newVector[i] << " ";
+  }
+  cout << endl;
 }
-
-//Exercise 10:
-//Create a function that takes an array of integers and returns the average of the even numbers from that array
